3a50b14c118ad1ff0e9a5cd2bf0955238815a05b
David Roundy <roundyd@physics.oregonstate.edu>
2014-02-24 17:50:44 -0800

    work on nice mannik plot (needs more work) plus tune colors on boxplot Tested everything on bennet.

--- paper/arrow-plot.py
+++ paper/arrow-plot.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.font_manager import FontProperties
 
+import matplotlib.image as mpimg
+
 #membrane.dat or arrow.dat printed transposed, gotta align them:
 
 dx=0.05
@@ -21,6 +23,31 @@ arg_set = ["randst-0.25-11.00-11.00-95.00-15.00","randst-0.25-20.00-20.00-95.00-
            "randst-0.25-23.00-33.00-94.00-15.00"]#"randst-0.25-30.00-40.00-94.00-15.00"]
 
 plt.figure()
+fig, ax = plt.subplots(figsize=(5,6))
+
+ax.set_frame_on(False)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+def plot_mannik():
+    m1=mpimg.imread('mannik-one.png')
+    m2=mpimg.imread('mannik-two.png')
+
+    dx = 7.0/380
+    y1 = -dx*np.arange(0, m1.shape[0])
+    x1 = dx*np.arange(0, m1.shape[1])
+    x1, y1 = np.meshgrid(x1, y1)
+    xcenter = x1[m1[:,:,0] > 0].mean()
+    yoff = 1.7
+    plt.contourf(x1-xcenter, y1-yoff, m1[:,:,1], levels=[0, 0.9], colors=('r', 'w'))
+    plt.contourf(x1-xcenter, y1-yoff, m1[:,:,0], levels=[0, 0.9], colors=('k', 'w'))
+
+    y2 = -dx*np.arange(0, m2.shape[0])
+    x2 = dx*np.arange(0, m2.shape[1])
+    x2, y2 = np.meshgrid(x2, y2)
+    xcenter = x2[m2[:,:,0] > 0].mean()
+    plt.contourf(8+x2-xcenter, y2-yoff, m2[:,:,1], levels=[0, 0.9], colors=('r', 'w'))
+    plt.contourf(8+x2-xcenter, y2-yoff, m2[:,:,0], levels=[0, 0.9], colors=('k', 'w'))
+plot_mannik()
 
 for i in range(len(arg_set)):
     cell_membrane_initial = np.loadtxt("./data/shape-randst/membrane_files/sections-%s.dat" % arg_set[i])
@@ -54,22 +81,25 @@ for i in range(len(arg_set)):
     if len(tails)==0:
         print "The tails in this file has no tails!!"
         exit(1)
-    print tails
     for k in range(len(tails[:,0])-1,0,-1):
         if tails[k,3] <0:
             tails = np.delete(tails,k,0)
-            print k
-        else:
-            print tails[k,3]
         if tails[k,2] < tails[k-1,2]+3:
             if tails[k,3] > tails[k-1,3]:
                 tails = np.delete(tails,k-1,0)
             else:
                 tails = np.delete(tails,k,0)
-    print tails
     tails = tails*dx
-    tails = tails[22:,:] # cut out just the first arrow
-    ax = plt.subplot(2,4,i)
+    tails = tails[15:,:] # cut out the first few arrows
+    ax = plt.subplot(1,1,1)
+    cell_membrane[cell_membrane>0] = 1
+    cell_x = np.linspace(0,dx*len(cell_membrane[0,:]),len(cell_membrane[0,:]))
+    cell_y = np.linspace(0,dx*len(cell_membrane[:,0]),len(cell_membrane[:,0]))
+    cell_x, cell_y = np.meshgrid(cell_x, cell_y)
+    centerx = cell_x[cell_membrane == 1].mean()
+    centery = cell_y[cell_membrane == 1].mean()
+    originx = (1 - (i//3))*8 - centerx
+    originy = (i%3)*3.5 + (i%3)**2*0.6 - centery
     ax.set_aspect('equal', 'datalim')
     font=FontProperties()
     font.set_family('serif')
@@ -79,21 +109,18 @@ for i in range(len(arg_set)):
         dir_y = (tails[j][1]-dx*Ny/2.0)/radial_length
         number_zpos = tails[j][0]+.18*dir_z
         number_ypos = tails[j][1]+.18*dir_y
-        ax.annotate('',xy=(tails[j+1][1],tails[j+1][0]),xytext=(tails[j][1],tails[j][0]),
+        ax.annotate('',xy=(originx+tails[j+1][1],originy+tails[j+1][0]),xytext=(originx+tails[j][1],originy+tails[j][0]),
                     fontsize=7,
                     arrowprops=dict(color='red',shrink=0.01, width=.3, headwidth=5.))
         #ax.text(number_zpos,number_ypos,"%d"%(j+1),fontsize=30,fontproperties=font,color='red')
-    cell_membrane[cell_membrane>0] = 1
-    cell_x = np.linspace(0,dx*len(cell_membrane[0,:]),len(cell_membrane[0,:]))
-    cell_y = np.linspace(0,dx*len(cell_membrane[:,0]),len(cell_membrane[:,0]))
-    plt.contour(cell_x,cell_y,cell_membrane, linewidths=2,levels=[.99])
+    plt.contour(originx+cell_x,originy+cell_y,cell_membrane, linewidths=2,levels=[.99])
     #plt.ax.quiver(X,Y,U,V,scale_units='xy',angles='xy',scale=1)
     ax.get_yaxis().set_visible(True)
     ax.get_xaxis().set_visible(True)
     #plt.xlim((0,dx*cell_membrane.shape[1]))
     #plt.ylim((0,dx*cell_membrane.shape[0]))
-    plt.xlabel("Z grid position")
-    plt.ylabel("Y grid position")
+    #plt.xlabel("Z grid position")
+    #plt.ylabel("Y grid position")
     #plt.title("Local temporal maxima, global spatial maxima view of %s"%(protein))
 plt.savefig("data/shape-randst/plots/paper-arrow-plot.pdf")
 
