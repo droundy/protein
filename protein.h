e372eb359b9cf9b9924a631b1517cc6919175ff3
jeffschulte <jeffschulte83@gmail.com>
2014-06-01 15:26:27 -0700

    commented out some unused functions from protein header

--- protein.h
+++ protein.h
@@ -67,15 +67,15 @@ bool inside(int xi, int yi, int zi);
 
 
 //protein_weights:
-void update_densities_and_weighting_for_reaction(stoch_params p, weights *ws, bool *insideArr, int *N_ATP, int *N_ADP,
-                                    int *N_E, int *ND_st, int *NDE_st, double *mem_A);
-void update_densities_and_weighting_for_diffusion(stoch_params p, weights *ws, bool *insideArr, int *N_ATP, int *N_ADP,
-                                    int *N_E, int *ND_st, int *NDE_st, double *mem_A);
 void initialize_densities_and_weighting(weights *ws, bool *insideArr, int *N_ATP, int *N_ADP, int *N_E,
                                         int *ND_st, int *NDE_st, double *mem_A);
 void update_all_densities_and_weighting_for_changing_gridpt(weights *ws, bool *insideArr, int *s_N_ATP,
                                                    int *s_N_ADP, int *s_N_E, int *s_ND, int *s_NDE, double *mem_A,
                                                    int xi, int yi, int zi);
+/* void update_densities_and_weighting_for_reaction(stoch_params p, weights *ws, bool *insideArr, int *N_ATP, int *N_ADP, */
+/*                                     int *N_E, int *ND_st, int *NDE_st, double *mem_A); */
+/* void update_densities_and_weighting_for_diffusion(stoch_params p, weights *ws, bool *insideArr, int *N_ATP, int *N_ADP, */
+/*                                     int *N_E, int *ND_st, int *NDE_st, double *mem_A); */
 
 
 
