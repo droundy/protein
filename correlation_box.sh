16b76a9eee541e5a48030516bbb1810f63fe382d
jeffschulte <jeffschulte83@gmail.com>
2014-09-08 14:58:47 -0700

    rewrote correlation data creation and correltation plot Tested everything on quipu.

--- correlation_box.sh
+++ correlation_box.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
 
 echo starting !
-# srun python density-correlation.py p 3.00 0.50 0.00 0.00 15.00 0.00 10.0 &
-# srun python density-correlation.py p 2.00 0.50 0.00 0.00 15.00 0.00 10.0 &
-# srun python density-correlation.py p 4.00 0.50 0.00 0.00 15.00 0.00 10.0 &
-# srun python density-correlation.py p 9.00 0.50 0.00 0.00 15.00 0.00 10.0 &
-# srun python density-correlation.py p 0.30 0.30 0.00 0.00 15.00 0.00 10.0 &
-# srun python density-correlation.py p 0.45 0.45 0.00 0.00 15.00 0.00 10.0 &
 
-# echo finished with the pills, now moving to the triangles
-# srun python density-correlation.py triangle 0.25 2.00 2.00 2.00 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 2.33 2.33 2.33 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 2.66 2.66 2.66 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 3.00 3.00 3.00 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 3.33 3.33 3.33 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 3.66 3.66 3.66 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 4.00 4.00 4.00 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 6.00 6.00 6.00 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 8.00 8.00 8.00 15.00 0.00 10.0 &
-# srun python density-correlation.py triangle 0.25 9.00 9.00 9.00 15.00 0.00 10.0 &
+# srun --job-name="corr randst 18.50 18.50 95.00 full_array 0.00 10.00" python pyplots/density-correlation.py randst 0.25 18.50 18.50 95.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr randst 18.50 18.50 95.00 exact 0.00 10.00" python pyplots/density-correlation.py randst 0.25 18.50 18.50 95.00 15.00 exact 0.00 10.00 &
 
+# srun --job-name="corr randst 18.60 28.60 94.00 full_array 0.00 10.00" python pyplots/density-correlation.py randst 0.25 18.60 28.60 94.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr randst 18.60 28.60 94.00 exact 0.00 10.00" python pyplots/density-correlation.py randst 0.25 18.60 28.60 94.00 15.00 exact 0.00 10.00 &
 
+# srun --job-name="corr stad 2.35 1.32 full 0.00 10.00" python pyplots/density-correlation.py stad 0.25 2.35 1.32 0.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr stad 2.35 1.32 exact 0.00 10.00" python pyplots/density-correlation.py stad 0.25 2.35 1.32 0.00 15.00 exact 0.00 10.00 &
 
-srun python density-correlation.py randst 0.25 10.00 17.00 94.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 12.00 19.00 94.00 15.00 0.00 10.00 &
+# srun --job-name="corr stad 2.92 1.18 full 0.00 10.00" python pyplots/density-correlation.py stad 0.25 2.92 1.18 0.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr stad 2.92 1.18 exact 0.00 10.00" python pyplots/density-correlation.py stad 0.25 2.92 1.18 0.00 15.00 exact 0.00 10.00 &
 
-srun python density-correlation.py randst 0.25 8.00 8.00 95.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 10.00 10.00 95.00 15.00 0.00 10.00 &
+# srun --job-name="corr p 3.00 0.50 full 0.00 10.00" python pyplots/density-correlation.py p 3.00 0.50 0.00 0.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr p 3.00 0.50 exact 0.00 10.00" python pyplots/density-correlation.py p 3.00 0.50 0.00 0.00 15.00 exact 0.00 10.00
 
-srun python density-correlation.py randst 0.25 15.00 15.00 95.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 17.00 17.00 95.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 18.50 18.50 95.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 21.00 21.00 95.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 25.00 25.00 95.00 15.00 0.00 10.00 &
+####################################################
+# srun --job-name="corr randst 18.50 18.50 95.00 full_array 0.00 10.00" python pyplots/correlation-plot.py randst 0.25 18.50 18.50 95.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr randst 18.50 18.50 95.00 exact 0.00 10.00" python pyplots/correlation-plot.py randst 0.25 18.50 18.50 95.00 15.00 exact 0.00 10.00 &
 
-srun python density-correlation.py randst 0.25 14.00 24.00 94.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 16.00 26.00 94.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 18.50 28.00 94.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 18.60 28.60 94.00 15.00 0.00 10.00 &
-srun python density-correlation.py randst 0.25 23.00 32.00 94.00 15.00 0.00 10.00 &
+# srun --job-name="corr randst 18.60 28.60 94.00 full_array 0.00 10.00" python pyplots/correlation-plot.py randst 0.25 18.60 28.60 94.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr randst 18.60 28.60 94.00 exact 0.00 10.00" python pyplots/correlation-plot.py randst 0.25 18.60 28.60 94.00 15.00 exact 0.00 10.00 &
 
-srun python density-correlation.py stad 0.25 5.00 2.50 0.00 15.00 0.00 10.00 &
-srun python density-correlation.py stad 0.25 6.00 2.00 0.00 15.00 0.00 10.00 &
+# srun --job-name="corr stad 2.35 1.32 full 0.00 10.00" python pyplots/correlation-plot.py stad 0.25 2.35 1.32 0.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr stad 2.35 1.32 exact 0.00 10.00" python pyplots/correlation-plot.py stad 0.25 2.35 1.32 0.00 15.00 exact 0.00 10.00 &
 
-srun python density-correlation.py p 4.00 0.50 0.00 0.00 15.00 0.00 10.00 &
+# srun --job-name="corr stad 2.92 1.18 full 0.00 10.00" python pyplots/correlation-plot.py stad 0.25 2.92 1.18 0.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr stad 2.92 1.18 exact 0.00 10.00" python pyplots/correlation-plot.py stad 0.25 2.92 1.18 0.00 15.00 exact 0.00 10.00 &
+
+# srun --job-name="corr p 3.00 0.50 full 0.00 10.00" python pyplots/correlation-plot.py p 3.00 0.50 0.00 0.00 15.00 full_array 0.00 10.00 &
+# srun --job-name="corr p 3.00 0.50 exact 0.00 10.00" python pyplots/correlation-plot.py p 3.00 0.50 0.00 0.00 15.00 exact 0.00 10.00
