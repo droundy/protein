d3a27984c40d981c4e74b16b49c40fae52ac4c0f
jeffschulte <jeffschulte83@gmail.com>
2014-09-18 09:31:50 -0700

    added new data for correlations Tested everything on quipu.

--- correlation_box.sh
+++ correlation_box.sh
@@ -17,6 +17,7 @@ srun --job-name="corr stad 2.92 1.18 exact 0.00 910.00" python pyplots/density-c
 srun --job-name="corr p 3.00 0.50 full 0.00 6000.00" python pyplots/density-correlation.py p 3.00 0.50 0.00 0.00 15.00 full_array 0.00 &
 srun --job-name="corr p 3.00 0.50 exact 0.00 6000.00" python pyplots/density-correlation.py p 3.00 0.50 0.00 0.00 15.00 exact 0.00
 
+
 ####################################################
 # srun --job-name="corr randst 18.50 18.50 95.00 full_array 0.00 10.00" python pyplots/correlation-plot.py randst 0.25 18.50 18.50 95.00 15.00 full_array 0.00 1125.00 rl &
 # srun --job-name="corr randst 18.50 18.50 95.00 exact 0.00 10.00" python pyplots/correlation-plot.py randst 0.25 18.50 18.50 95.00 15.00 exact 0.00 1125.00 rl &
