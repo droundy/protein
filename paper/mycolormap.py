199c320c1617677bf1563f8a0df5841250c850ea
David Roundy <roundyd@physics.oregonstate.edu>
2014-09-19 12:37:09 -0700

    tweak colors to make movie clearer maybe Tested everything on bennet.

--- paper/mycolormap.py
+++ paper/mycolormap.py
@@ -3,7 +3,8 @@ import matplotlib
 
 cdata = np.array([[0  ,1,1,1],
                   [.02 ,1,1,1],
-                  [.2,0.2,.2,1],
+                  [.05,0.7,.3,1],
+                  [.15,0.2,.2,1],
                   [.3 ,0,.8,0],
                   [.4 ,1,1,0],
                   [.6 ,1,0,0],
@@ -17,4 +18,4 @@ cmap = matplotlib.colors.LinearSegmentedColormap('mine', cdict)
 
 pancake_levels = np.arange(0, 961, 20)
 pill_levels = np.arange(0, 2001, 50)
-movie_levels = np.arange(0, 1801, 20)
+movie_levels = np.arange(0, 1801, 10)
