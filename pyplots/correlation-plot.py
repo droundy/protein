fcbacd8f787c2483867387bd784e3bd07280f6b6
jeffschulte <jeffschulte83@gmail.com>
2015-03-27 14:16:05 -0700

    address referees comments Tested everything on crystal.

--- pyplots/correlation-plot.py
+++ pyplots/correlation-plot.py
@@ -116,7 +116,7 @@ for i in range(1,num_plots):
     print 'period ',period
     pylab.plot(time_array,data_full[i],colors[i],label='stochastic')
     pylab.xlim(0,time_array[-1])
-    #pylab.ylim(np.amin(data_full[i]),np.amax(data_full[i]))
+    pylab.ylim(1.1*np.amin(data_full[i]),1.1*np.amax(data_full[i]))
 
 if not plot_final_version:
     pylab.plot(time_array,decay,colors[0],label='decay')
@@ -124,17 +124,21 @@ if not plot_final_version:
 pylab.title(r'%s %s $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
             % (sys.argv[1], sys.argv[5], 1/rate, period, 1/(rate*period)))
 if sys.argv[1] == 'randst' and sys.argv[5] == '95.00':
-    pylab.title(r'mannik A, $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
-                % (1/rate, period, 1/(rate*period)))
-    #pylab.title(r'Temporal correlation of shape A')
+    #pylab.title(r'mannik A, $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
+    #            % (1/rate, period, 1/(rate*period)))
+    pylab.title(r'Temporal correlation of shape A')
+elif sys.argv[1] == 'randst' and sys.argv[5] == '94.00':
+    pylab.title(r'Temporal correlation of shape B')
 elif sys.argv[1] == 'stad' and sys.argv[4] == '1.32':
-    pylab.title(r'stadium A, $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
-                % (1/rate, period, 1/(rate*period)))
-    #pylab.title(r'Temporal correlation of stadium A')
+    #pylab.title(r'stadium A, $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
+    #            % (1/rate, period, 1/(rate*period)))
+    pylab.title(r'Temporal correlation of stadium A')
+elif sys.argv[1] == 'stad' and sys.argv[4] == '1.18':
+    pylab.title(r'Temporal correlation of stadium B')
 elif sys.argv[1] == 'p':
-    pylab.title(r'pill-shaped cell, $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
-                % (1/rate, period, 1/(rate*period)))
-    #pylab.title(r'Temporal correlation of wild-type pill shape')
+    #pylab.title(r'pill-shaped cell, $\tau_c = %.3g$ sec $T = %.3g$ sec (ratio %.2g)'
+    #            % (1/rate, period, 1/(rate*period)))
+    pylab.title(r'Temporal correlation of wild-type pill shape')
 
 
 
